90ac5f280f3a3b6d95ca23534d6ad394c6125508
Christofer <christoferguima@gmail.comnam>
2022-03-04 15:20:40 -0300

    added input and button to search for a city

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,21 +12,30 @@ interface CityData {
 
 function App() {
   const [cityData, setCityData] = useState<CityData>();
+  const [city, setCity] = useState<String>("");
 
-  useEffect(() => {
-    getDataCity("London")
+  function getWeather() {
+    getDataCity(city)
       .then(({ data }) => setCityData(data.list[0].main))
       .catch((err) => console.log(err));
-  }, []);
+  }
 
   return (
     <div className="App">
-      <div>{cityData?.temp}</div>
-      <div>{cityData?.feels_like}</div>
-      <div>{cityData?.humidity}</div>
-      <div>{cityData?.pressure}</div>
-      <div>{cityData?.temp_max}</div>
-      <div>{cityData?.temp_min}</div>
+      <input
+        type="text"
+        placeholder="City name.."
+        onChange={(e) => setCity(e.target.value)}
+      ></input>
+      <button onClick={() => getWeather()}>Click me!</button>
+      <ul className="city-data-container">
+        <li>{cityData?.temp}</li>
+        <li>{cityData?.feels_like}</li>
+        <li>{cityData?.humidity}</li>
+        <li>{cityData?.pressure}</li>
+        <li>{cityData?.temp_max}</li>
+        <li>{cityData?.temp_min}</li>
+      </ul>
     </div>
   );
 }
