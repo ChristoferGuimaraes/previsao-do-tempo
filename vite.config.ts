cb0dafceb6c7e202d87d914b2d567ea0184ba70a
Christofer <christoferguima@gmail.comnam>
2022-03-22 15:24:25 -0300

    refactoring css

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { env } from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: false
-  },
-  server: {
-    port: Number(process.env.PORT) || 3030
   },
   plugins: [react()]
 })
